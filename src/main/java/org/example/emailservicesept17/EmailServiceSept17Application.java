428e1384deaf3db5e8023e27aa0c0bb591272f07
khannaanurag1 <khannaanurag1@gmail.com>
2024-09-17 20:42:35 +0530

    Created Spring Application

---
+++ src/main/java/org/example/emailservicesept17/EmailServiceSept17Application.java
@@ -0,0 +1,13 @@
+package org.example.emailservicesept17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceSept17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceSept17Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/emailservicesept17/EmailServiceSept17ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.emailservicesept17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceSept17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
